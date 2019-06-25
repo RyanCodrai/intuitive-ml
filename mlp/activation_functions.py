a6b402606b91b7ce4a0f11dfc7aae8604dc5cc01
Ryan Codrai <ryan@Ryans-MacBook-Pro.local>
2019-06-25 12:38:12 +0100

    Decouple Relu and Sigmoid activation functions and use code that is more self descriptive for Relu

--- mlp/activation_functions.py
+++ mlp/activation_functions.py
@@ -4,16 +4,12 @@ class sigmoid():
 	def __call__(self, z):
 		return 1 / (1 + np.exp(-z))
 
-	def derivative(self, dA, Z):
-		sig = sigmoid()
-		sig = sig(Z)
-		return dA * sig * (1 - sig)
+	def derivative(self, z):
+		return self.__call__(z) * (1 - self.__call__(z))
 
 class relu():
 	def __call__(self, z):
 		return np.maximum(0, z)
 
-	def derivative(self, dA, Z):
-		dZ = np.array(dA, copy = True)
-		dZ[Z <= 0] = 0;
-		return dZ;
+	def derivative(self, z):
+		return np.greater(z, 0).astype(int)
