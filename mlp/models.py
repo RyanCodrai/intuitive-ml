b0e18dac9bd8441ec561f23e6ea4fd8d2343fbbf
Ryan Codrai <ryan@Ryans-MacBook-Pro.local>
2019-06-27 12:14:26 +0100

    Remove input_shape for layer objects

--- mlp/models.py
+++ mlp/models.py
@@ -8,10 +8,6 @@ class Sequential:
 		if self.layers is None and layer.input_shape is None:
 			raise Exception('The first layer in a Sequential model must get an `input_shape`')
 
-		# Set the input shape for this new layer to be equal to the output shape from the previous layer
-		if self.layers:
-			layer.input_shape = self.layers[-1].units
-
 		# Add the layer to the neural network
 		self.layers.append(layer)
 
